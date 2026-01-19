e2bb77435f545e34d562954e7dbc9dc37c9fca6f
Jacek Schae <jacek.schae@gmail.com>
2026-01-19 07:32:52 +0100

    ci: add GitHub Actions for CI/CD and daily checks
    - Configure workflows for testing, releases, and automated Stripe API updates (`test.yml`, `release.yml`, `update-api.yml`).
    - Add `git-cliff` for changelog generation.
    - Update `deps.edn` with deployment support (`deps-deploy`).
    - Enhance `build.clj` for versioning and deployment to Clojars (`deploy` and `jar` updates).

--- src/dev/build.clj
+++ src/dev/build.clj
@@ -2,61 +2,77 @@
   (:refer-clojure :exclude [compile])
   (:require
     [clojure.tools.build.api :as b]
+    [deps-deploy.deps-deploy :as dd]
     [versions :as v]))
 
 (def lib 'stream.clojure/stripe-clojure)
-(def version (format "stripe-%s/clojure-sdk-0.0.%s-alpha" v/stripe-api-version (b/git-count-revs nil)))
 (def class-dir "target/classes")
-(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn- version-string
+  "Generates version string. If :version is provided, uses it directly.
+   Otherwise generates from Stripe API version and git commit count."
+  [opts]
+  (or (:version opts)
+      (format "stripe-%s/clojure-sdk-0.0.%s-alpha"
+              v/stripe-api-version
+              (b/git-count-revs nil))))
+
+(defn- jar-file [version]
+  (format "target/%s-%s.jar" (name lib) version))
 
 (def basis (delay (b/create-basis {:project "deps.edn"})))
 
-(defn write-version-file []
-  (let [version-file "docs/VERSION"]
-    (spit version-file version)
-    (println "Wrote version to" version-file)))
-
-(defn clean [_]
+(defn clean
+  "Delete the target directory."
+  [_]
   (b/delete {:path "target"}))
 
-(defn compile [_]
-  (clean nil)
-  (b/copy-dir {:src-dirs ["src"]
-               :target-dir class-dir})
-  (b/compile-clj {:basis @basis
-                  :src-dirs ["src/main"]
-                  :class-dir class-dir}))
-
-(defn jar-pom-and-copy []
-  (b/write-pom {:class-dir class-dir
-                :lib lib
-                :version version
-                :basis @basis
-                :src-dirs ["src/main"]})
+(defn compile
+  "Compile Clojure source files."
+  [_]
   (b/copy-dir {:src-dirs ["src/main"]
                :target-dir class-dir}))
 
-(defn jar [_]
-  (compile nil)
-  (jar-pom-and-copy)
-  (b/jar {:class-dir class-dir
-          :jar-file jar-file})
-  (write-version-file)
-  (println "Built jar:" jar-file))
+(defn jar
+  "Build the JAR file.
+   Options:
+     :version - Version string (optional, auto-generated if not provided)"
+  [opts]
+  (clean nil)
+  (let [version (version-string opts)
+        jar-path (jar-file version)]
+    (compile nil)
+    (b/write-pom {:class-dir class-dir
+                  :lib lib
+                  :version version
+                  :basis @basis
+                  :src-dirs ["src/main"]
+                  :scm {:url "https://github.com/clojurestream/stripe-clojure"
+                        :connection "scm:git:git://github.com/clojurestream/stripe-clojure.git"
+                        :developerConnection "scm:git:ssh://git@github.com/clojurestream/stripe-clojure.git"
+                        :tag (str "v" version)}
+                  :pom-data [[:description "Clojure SDK for the Stripe API"]
+                             [:url "https://github.com/clojurestream/stripe-clojure"]
+                             [:licenses
+                              [:license
+                               [:name "MIT License"]
+                               [:url "https://opensource.org/licenses/MIT"]]]]})
+    (b/copy-dir {:src-dirs ["src/main"]
+                 :target-dir class-dir})
+    (b/jar {:class-dir class-dir
+            :jar-file jar-path})
+    (println "Built JAR:" jar-path)
+    {:jar-file jar-path :version version}))
 
-#_(defn deploy [_]
-  (jar nil)
-  (let [username (System/getenv "CLOJARS_USERNAME")
-        password (System/getenv "CLOJARS_PASSWORD")
-        deploy-url "https://clojars.org/repo/"
-        response (http/post deploy-url
-                   {:multipart [{:name "artifact"
-                                 :file jar-file
-                                 :mime-type "application/java-archive"}
-                                {:name "pom"
-                                 :file pom-file
-                                 :mime-type "text/xml"}]
-                    :basic-auth [username password]
-                    :throw-exceptions false})]
-    (println "Deploy response:" response)
-    (println "Deployed version:" version)))
+(defn deploy
+  "Deploy to Clojars.
+   Options:
+     :version - Version string (optional, auto-generated if not provided)
+
+   Requires CLOJARS_USERNAME and CLOJARS_PASSWORD environment variables."
+  [opts]
+  (let [{:keys [jar-file version]} (jar opts)]
+    (dd/deploy {:installer :remote
+                :artifact (b/resolve-path jar-file)
+                :pom-file (b/pom-path {:lib lib :class-dir class-dir})})
+    (println "Deployed to Clojars:" lib version)))
